cff6181aa177cf0ef663cf21f2d10713c037cac9
WeirdApple <71232000+WeirdApple@users.noreply.github.com>
2021-03-05 14:10:42 -0500

    moderation cog start

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+import time
+import random
+from random import choice
+import asyncio
+from discord.ext import commands, tasks
+from discord.utils import find, get
+import os
+import io
+import datetime
+
+
+status = ["me getting coded again","Almost got wiped from existence","Almost dead"]
+
+prefixes = ['E/', 'e/']
+client = commands.Bot(command_prefix = prefixes, guild_subscriptions = True, intents = discord.Intents.all())
+client.remove_command("help")
+
+@tasks.loop(seconds = 20)
+async def change_status():
+	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= random.choice(status)))
+
+@client.event
+async def on_ready():
+	change_status.start()
+	print("bot is ready")
+
+
+#cogs
+initial_extensions = ['cogs.moderation',
+                      'cogs.fun',
+                      'cogs.events',
+					  'cogs.other']
+
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        client.load_extension(extension)
+
+
+
+
+client.run("Nzc5MDQ5ODc5MzQ3MjY1NTM2.X7a4vQ.iNMYId3va8G-J3f92ZanlnSKzZA", bot = True, reconnect = True)
